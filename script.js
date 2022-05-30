09f90bfce7ec956bd8304b3d7df7cb6fc7ec139a
Nick <Nick_II2000@mail.ru>
2022-05-30 21:26:25 +0600

    2-th commit

--- script.js
+++ script.js
@@ -2,15 +2,26 @@
 
 // const line = Symbol('line');
 
-const user = {
-    4: 'Four',
-    name: 'Alex',
-    surname: 'Smith',
-    birthday: '20/04/1993',
-    showMyPublicData: function () {
-        console.log(`${this.name} ${this.surname}`);
-    },
-};
+// const user = {
+//     4: 'Four',
+//     name: 'Alex',
+//     surname: 'Smith',
+//     birthday: '20/04/1993',
+//     showMyPublicData: function () {
+//         console.log(`${this.name} ${this.surname}`);
+//     },
+// };
 
-console.log(Object.keys(user));
-console.log(typeof (Object.keys(user))[0]);
+// console.log(Object.keys(user));
+// console.log(typeof (Object.keys(user))[0]);
+
+const shops = [
+    { rice: 500 },
+    { oil: 200 },
+    { bread: 50 },
+];
+
+const map = new Map();
+
+map.set(shops[0], 5000);
+console.log(map);
